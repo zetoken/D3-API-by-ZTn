f840c425dab8933ac36611bd3e551625ff43b7f8
ZeToken <github@zetoken.net>
2012-09-18 10:53:14 +0200

    - Squeleton for D3 Calculator Library

---
+++ D3 Calculator Library by ZTn/D3Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ZTn.BNet.D3.Items;
+
+namespace ZTn.BNet.D3.Calculator
+{
+    public class D3Calculator
+    {
+        Item[] items;
+
+        public D3Calculator(Item[] item)
+        {
+
+        }
+    }
+}
