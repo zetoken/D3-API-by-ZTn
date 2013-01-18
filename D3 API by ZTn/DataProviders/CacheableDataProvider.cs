d7c276113596e8f7fca823b1e9642652d7fb9e4f
ZeToken <github@zetoken.net>
2013-01-18 10:29:21 +0100

    - D3API: CacheableDataProvider updated (cache directories)

--- D3 API by ZTn/DataProviders/CacheableDataProvider.cs
+++ D3 API by ZTn/DataProviders/CacheableDataProvider.cs
@@ -38,20 +38,26 @@ namespace ZTn.BNet.D3.DataProviders
             StringBuilder stringBuilder = new StringBuilder();
 
             Uri uri = new Uri(url);
+
+            stringBuilder.Append(uri.Host + "/");
+
             if (uri.LocalPath.Contains("/profile/"))
             {
+                // Example: http://eu.battle.net/api/d3/profile/Tok-2360 >> eu.battle.net/Tok-2360.json
                 String[] splitted = uri.LocalPath.Split(new String[] { "/profile/" }, StringSplitOptions.None);
                 stringBuilder.Append(splitted[1]);
                 stringBuilder.Append(".json");
             }
             else if (uri.LocalPath.Contains("/hero/"))
             {
+                // Example: http://eu.battle.net/api/d3/profile/Tok-2360/hero/17023 >> eu.battle.net/Tok-2360/hero/17023.json
                 String[] splitted = uri.LocalPath.Split(new String[] { "/hero/" }, StringSplitOptions.None);
                 stringBuilder.Append(splitted[1]);
                 stringBuilder.Append(".json");
             }
             else if (uri.LocalPath.Contains("/item/"))
             {
+                // Example: http://eu.battle.net/api/d3/data/item/Amethyst_14 >> eu.battle.net/items/Amethyst_14.json (or a hash)
                 stringBuilder.Append("items/");
                 String[] splitted = uri.LocalPath.Split(new String[] { "/item/" }, StringSplitOptions.None);
                 if (splitted[1].Length < 32)
@@ -72,6 +78,7 @@ namespace ZTn.BNet.D3.DataProviders
             }
             else if (uri.LocalPath.Contains("/icons/"))
             {
+                // Example: http://media.blizzard.com/d3/icons/items/small/amethyst_12_demonhunter_male.png >> media.blizzard.com/icons/items/small/amethyst_12_demonhunter_male.png
                 stringBuilder.Append("icons/");
                 String[] splitted = uri.LocalPath.Split(new String[] { "/icons/" }, StringSplitOptions.None);
                 stringBuilder.Append(splitted[1]);
@@ -91,10 +98,14 @@ namespace ZTn.BNet.D3.DataProviders
             return stringBuilder.ToString();
         }
 
+        public virtual String getCacheStoragePath()
+        {
+            return "cache/";
+        }
+
         public Stream fetchData(string url)
         {
-            String cachedFilePath = "cache/" + getCachedFileName(url);
-
+            String cachedFilePath = getCacheStoragePath() + getCachedFileName(url);
 
             if (online)
             {
